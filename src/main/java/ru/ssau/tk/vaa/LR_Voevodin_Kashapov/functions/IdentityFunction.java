2686c26f56ed5c8daeeaa5290489236bec438bff
Anton <voevodin_anton_2000@mail.ru>
2020-09-16 18:41:13 +0400

    функция apply() покрыта тестами

---
+++ src/main/java/ru/ssau/tk/vaa/LR_Voevodin_Kashapov/functions/IdentityFunction.java
@@ -0,0 +1,8 @@
+package ru.ssau.tk.vaa.LR_Voevodin_Kashapov.functions;
+
+public class IdentityFunction implements MathFunction {
+    @Override
+    public double apply(double x) {
+        return x;
+    }
+}
---
+++ src/test/java/ru/ssau/tk/vaa/LR_Voevodin_Kashapov/functions/IdentityFunctionTest.java
@@ -0,0 +1,17 @@
+package ru.ssau.tk.vaa.LR_Voevodin_Kashapov.functions;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class IdentityFunctionTest {
+    @Test
+    public void TestMathFunction() {
+        IdentityFunction function = new IdentityFunction();
+
+        assertEquals(function.apply(4), 4.0);
+        assertEquals(function.apply(0), 0.0);
+        assertEquals(function.apply(-10), -10.0);
+        assertEquals(function.apply(1/10.0), 0.1);
+    }
+}
