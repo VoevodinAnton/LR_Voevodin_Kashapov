a4481fc11fcc3cb77672427cc60e5de54ce144e9
Artem Kashapov <ar.kashapov@gmail.com>
2020-09-17 12:43:59 +0400

    Added class SqrFunction

---
+++ src/main/java/ru/ssau/tk/vaa/LR_Voevodin_Kashapov/functions/SqrFunction.java
@@ -0,0 +1,9 @@
+package ru.ssau.tk.vaa.LR_Voevodin_Kashapov.functions;
+
+public class SqrFunction implements MathFunction {
+
+    @Override
+    public double apply(double x) {
+        return x*x;
+    }
+}
