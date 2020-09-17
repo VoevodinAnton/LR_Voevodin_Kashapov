16910590db12c2a41c74a912c6150fbeecb4a35d
Artem Kashapov <ar.kashapov@gmail.com>
2020-09-17 12:51:44 +0400

    Created test for SqrFunction

---
+++ src/test/java/ru/ssau/tk/vaa/LR_Voevodin_Kashapov/functions/SqrFunctionTest.java
@@ -0,0 +1,22 @@
+package ru.ssau.tk.vaa.LR_Voevodin_Kashapov.functions;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class SqrFunctionTest {
+    @Test
+    public void sqrFunctionTest() {
+        SqrFunction x = new SqrFunction();
+
+        assertEquals(x.apply(2), 4.0);
+
+        assertEquals(x.apply(3), 9.0);
+
+        assertEquals(x.apply(4), 16.0);
+
+        assertEquals(x.apply(5), 25.0);
+
+        assertEquals(x.apply(6), 36.0);
+    }
+}
