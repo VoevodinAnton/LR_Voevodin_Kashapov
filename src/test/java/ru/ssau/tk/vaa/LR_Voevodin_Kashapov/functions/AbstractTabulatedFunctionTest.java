d878195f72caebb90de15dca1cae780c95063c52
Artem Kashapov <ar.kashapov@gmail.com>
2020-10-07 18:09:53 +0400

    Красивация

--- src/test/java/ru/ssau/tk/vaa/LR_Voevodin_Kashapov/functions/AbstractTabulatedFunctionTest.java
+++ src/test/java/ru/ssau/tk/vaa/LR_Voevodin_Kashapov/functions/AbstractTabulatedFunctionTest.java
@@ -14,11 +14,11 @@ public class AbstractTabulatedFunctionTest {
         assertEquals(mock.interpolate(2.0, 1.0, 5.0, 4.0, 2.0), 3.5, ACCURACY);
         assertEquals(mock.interpolate(5.0, 3.0, 7.0, 6.0, 8.0), 7, ACCURACY);
         assertEquals(mock.interpolate(3.0, 2.71828182854, 3.1415926535, 3.1415926535, 2.71828182854), 2.859874482040000, ACCURACY);
-        assertEquals(mock.interpolate(9.0, 2.71828182854*2.71828182854, 3.1415926535*3.1415926535, 3.1415926535, 2.71828182854), 2.866681662562116, ACCURACY);
+        assertEquals(mock.interpolate(9.0, 2.71828182854 * 2.71828182854, 3.1415926535 * 3.1415926535, 3.1415926535, 2.71828182854), 2.866681662562116, ACCURACY);
         assertEquals(mock.interpolate(2.0, 1.0, 3.0, 10.0, 10.0), 10, ACCURACY);
     }
 
     @Test
-    public void applyTest(){
+    public void applyTest() {
     }
 }
