8210ef2ca43e59138867b7b70f2fd313ed831739
Artem Kashapov <ar.kashapov@gmail.com>
2020-10-08 12:48:20 +0400

    Исправлен тест

--- src/test/java/ru/ssau/tk/vaa/LR_Voevodin_Kashapov/functions/ArrayTabulatedFunctionTest.java
+++ src/test/java/ru/ssau/tk/vaa/LR_Voevodin_Kashapov/functions/ArrayTabulatedFunctionTest.java
@@ -22,13 +22,29 @@ public class ArrayTabulatedFunctionTest {
     private final double[] x2 = new double[]{1, 2, 3};
     private final double[] y2 = new double[]{1, 2, 1};
 
+    private ArrayTabulatedFunction arr1() {
+        return new ArrayTabulatedFunction(source, xFrom, xTo, count);
+    }
+
+    private ArrayTabulatedFunction arr2() {
+        return new ArrayTabulatedFunction(x, y);
+    }
+
+    private ArrayTabulatedFunction arr3() {
+        return new ArrayTabulatedFunction(x1, y1);
+    }
+
+    private ArrayTabulatedFunction arr4() {
+        return new ArrayTabulatedFunction(x2, y2);
+    }
 
     @Test
     public void testGetCount() {
-        AbstractTabulatedFunction t21 = new ArrayTabulatedFunction(source, xFrom, xTo, count);
-        AbstractTabulatedFunction t11 = new ArrayTabulatedFunction(x, y);
-        AbstractTabulatedFunction t12 = new ArrayTabulatedFunction(x1, y1);
-        AbstractTabulatedFunction t13 = new ArrayTabulatedFunction(x2, y2);
+        ArrayTabulatedFunction t21 = arr1();
+        ArrayTabulatedFunction t11 = arr2();
+        ArrayTabulatedFunction t12 = arr3();
+        ArrayTabulatedFunction t13 = arr4();
+
         assertEquals(t11.getCount(),
                 4);
         assertEquals(t12.getCount(),
@@ -40,10 +56,10 @@ public class ArrayTabulatedFunctionTest {
 
     @Test
     public void testGetX() {
-        AbstractTabulatedFunction t21 = new ArrayTabulatedFunction(source, xFrom, xTo, count);
-        AbstractTabulatedFunction t11 = new ArrayTabulatedFunction(x, y);
-        AbstractTabulatedFunction t12 = new ArrayTabulatedFunction(x1, y1);
-        AbstractTabulatedFunction t13 = new ArrayTabulatedFunction(x2, y2);
+        ArrayTabulatedFunction t21 = arr1();
+        ArrayTabulatedFunction t11 = arr2();
+        ArrayTabulatedFunction t12 = arr3();
+        ArrayTabulatedFunction t13 = arr4();
         assertEquals(t13.getX(0),
                 1.0);
         assertEquals(t13.getX(1),
@@ -62,8 +78,8 @@ public class ArrayTabulatedFunctionTest {
 
     @Test
     public void testGetY() {
-        AbstractTabulatedFunction t21 = new ArrayTabulatedFunction(source, xFrom, xTo, count);
-        AbstractTabulatedFunction t13 = new ArrayTabulatedFunction(x2, y2);
+        ArrayTabulatedFunction t21 = arr1();
+        ArrayTabulatedFunction t13 = arr4();
         assertEquals(t13.getY(0),
                 1.0);
         assertEquals(t13.getY(1),
@@ -76,9 +92,9 @@ public class ArrayTabulatedFunctionTest {
 
     @Test
     public void testSetY() {
-        AbstractTabulatedFunction t21 = new ArrayTabulatedFunction(source, xFrom, xTo, count);
-        AbstractTabulatedFunction t11 = new ArrayTabulatedFunction(x, y);
-        AbstractTabulatedFunction t13 = new ArrayTabulatedFunction(x2, y2);
+        ArrayTabulatedFunction t21 = arr1();
+        ArrayTabulatedFunction t11 = arr2();
+        ArrayTabulatedFunction t13 = arr4();
         t13.setY(2, 21);
         assertEquals(t13.getY(2),
                 21.0);
@@ -95,10 +111,10 @@ public class ArrayTabulatedFunctionTest {
 
     @Test
     public void testLeftBound() {
-        AbstractTabulatedFunction t21 = new ArrayTabulatedFunction(source, xFrom, xTo, count);
-        AbstractTabulatedFunction t11 = new ArrayTabulatedFunction(x, y);
-        AbstractTabulatedFunction t12 = new ArrayTabulatedFunction(x1, y1);
-        AbstractTabulatedFunction t13 = new ArrayTabulatedFunction(x2, y2);
+        ArrayTabulatedFunction t21 = arr1();
+        ArrayTabulatedFunction t11 = arr2();
+        ArrayTabulatedFunction t12 = arr3();
+        ArrayTabulatedFunction t13 = arr4();
         assertEquals(t11.leftBound(),
                 1.0);
         assertEquals(t12.leftBound(),
@@ -111,10 +127,10 @@ public class ArrayTabulatedFunctionTest {
 
     @Test
     public void testRightBound() {
-        AbstractTabulatedFunction t21 = new ArrayTabulatedFunction(source, xFrom, xTo, count);
-        AbstractTabulatedFunction t11 = new ArrayTabulatedFunction(x, y);
-        AbstractTabulatedFunction t12 = new ArrayTabulatedFunction(x1, y1);
-        AbstractTabulatedFunction t13 = new ArrayTabulatedFunction(x2, y2);
+        ArrayTabulatedFunction t21 = arr1();
+        ArrayTabulatedFunction t11 = arr2();
+        ArrayTabulatedFunction t12 = arr3();
+        ArrayTabulatedFunction t13 = arr4();
         assertEquals(t11.rightBound(),
                 16.0);
         assertEquals(t12.rightBound(),
@@ -127,10 +143,10 @@ public class ArrayTabulatedFunctionTest {
 
     @Test
     public void testIndexOfX() {
-        AbstractTabulatedFunction t21 = new ArrayTabulatedFunction(source, xFrom, xTo, count);
-        AbstractTabulatedFunction t11 = new ArrayTabulatedFunction(x, y);
-        AbstractTabulatedFunction t12 = new ArrayTabulatedFunction(x1, y1);
-        AbstractTabulatedFunction t13 = new ArrayTabulatedFunction(x2, y2);
+        ArrayTabulatedFunction t21 = arr1();
+        ArrayTabulatedFunction t11 = arr2();
+        ArrayTabulatedFunction t12 = arr3();
+        ArrayTabulatedFunction t13 = arr4();
         assertEquals(t11.indexOfX(9),
                 2);
         assertEquals(t12.indexOfX(20),
@@ -143,10 +159,10 @@ public class ArrayTabulatedFunctionTest {
 
     @Test
     public void testIndexOfY() {
-        AbstractTabulatedFunction t21 = new ArrayTabulatedFunction(source, xFrom, xTo, count);
-        AbstractTabulatedFunction t11 = new ArrayTabulatedFunction(x, y);
-        AbstractTabulatedFunction t12 = new ArrayTabulatedFunction(x1, y1);
-        AbstractTabulatedFunction t13 = new ArrayTabulatedFunction(x2, y2);
+        ArrayTabulatedFunction t21 = arr1();
+        ArrayTabulatedFunction t11 = arr2();
+        ArrayTabulatedFunction t12 = arr3();
+        ArrayTabulatedFunction t13 = arr4();
         assertEquals(t11.indexOfY(256),
                 3);
         assertEquals(t12.indexOfY(10),
@@ -159,9 +175,10 @@ public class ArrayTabulatedFunctionTest {
 
     @Test
     public void testFloorIndexOfX() {
-        AbstractTabulatedFunction t11 = new ArrayTabulatedFunction(x, y);
-        AbstractTabulatedFunction t12 = new ArrayTabulatedFunction(x1, y1);
-        AbstractTabulatedFunction t13 = new ArrayTabulatedFunction(x2, y2);
+        ArrayTabulatedFunction t21 = arr1();
+        ArrayTabulatedFunction t11 = arr2();
+        ArrayTabulatedFunction t12 = arr3();
+        ArrayTabulatedFunction t13 = arr4();
         assertEquals(t11.floorIndexOfX(7),
                 1);
         assertEquals(t12.floorIndexOfX(4),
@@ -172,10 +189,10 @@ public class ArrayTabulatedFunctionTest {
 
     @Test
     public void testInterpolate() {
-        AbstractTabulatedFunction t21 = new ArrayTabulatedFunction(source, xFrom, xTo, count);
-        AbstractTabulatedFunction t11 = new ArrayTabulatedFunction(x, y);
-        AbstractTabulatedFunction t12 = new ArrayTabulatedFunction(x1, y1);
-        AbstractTabulatedFunction t13 = new ArrayTabulatedFunction(x2, y2);
+        ArrayTabulatedFunction t21 = arr1();
+        ArrayTabulatedFunction t11 = arr2();
+        ArrayTabulatedFunction t12 = arr3();
+        ArrayTabulatedFunction t13 = arr4();
         assertEquals(t11.interpolate(10, 3),
                 106.0);
         assertEquals(t12.interpolate(15, 1),
@@ -188,10 +205,10 @@ public class ArrayTabulatedFunctionTest {
 
     @Test
     public void testExtrapolateLeft() {
-        AbstractTabulatedFunction t21 = new ArrayTabulatedFunction(source, xFrom, xTo, count);
-        AbstractTabulatedFunction t11 = new ArrayTabulatedFunction(x, y);
-        AbstractTabulatedFunction t12 = new ArrayTabulatedFunction(x1, y1);
-        AbstractTabulatedFunction t13 = new ArrayTabulatedFunction(x2, y2);
+        ArrayTabulatedFunction t21 = arr1();
+        ArrayTabulatedFunction t11 = arr2();
+        ArrayTabulatedFunction t12 = arr3();
+        ArrayTabulatedFunction t13 = arr4();
         assertEquals(t11.extrapolateLeft(0),
                 -4.0);
         assertEquals(t12.extrapolateLeft(5),
@@ -204,10 +221,10 @@ public class ArrayTabulatedFunctionTest {
 
     @Test
     public void testExtrapolateRight() {
-        AbstractTabulatedFunction t21 = new ArrayTabulatedFunction(source, xFrom, xTo, count);
-        AbstractTabulatedFunction t11 = new ArrayTabulatedFunction(x, y);
-        AbstractTabulatedFunction t12 = new ArrayTabulatedFunction(x1, y1);
-        AbstractTabulatedFunction t13 = new ArrayTabulatedFunction(x2, y2);
+        ArrayTabulatedFunction t21 = arr1();
+        ArrayTabulatedFunction t11 = arr2();
+        ArrayTabulatedFunction t12 = arr3();
+        ArrayTabulatedFunction t13 = arr4();
         assertEquals(t11.extrapolateRight(20),
                 356.0);
         assertEquals(t12.extrapolateRight(30),
