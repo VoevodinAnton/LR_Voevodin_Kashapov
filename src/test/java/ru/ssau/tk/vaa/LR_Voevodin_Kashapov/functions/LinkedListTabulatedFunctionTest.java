d20e881f3327ab5769b40d3f53ace60bb289e435
Anton <voevodin_anton_2000@mail.ru>
2020-11-05 13:23:25 +0400

    АВТОМАТическое обновление v9.

--- src/test/java/ru/ssau/tk/vaa/LR_Voevodin_Kashapov/functions/LinkedListTabulatedFunctionTest.java
+++ src/test/java/ru/ssau/tk/vaa/LR_Voevodin_Kashapov/functions/LinkedListTabulatedFunctionTest.java
@@ -41,9 +41,18 @@ public class LinkedListTabulatedFunctionTest {
             LinkedListTabulatedFunction list = new LinkedListTabulatedFunction(new double[]{1}, new double[]{2});
         });
 
+        assertThrows(MyAssertions.assertDoesNotThrow(() -> {
+            LinkedListTabulatedFunction list = new LinkedListTabulatedFunction(new double[]{1, 3}, new double[]{2, 4});
+        }));
+
         assertThrows(IllegalArgumentException.class, () -> {
             LinkedListTabulatedFunction list = new LinkedListTabulatedFunction(function, 3, 1, 10);
         });
+
+        assertThrows(MyAssertions.assertDoesNotThrow(() -> {
+            LinkedListTabulatedFunction list = new LinkedListTabulatedFunction(function, 1, 3, 10);
+        }));
+
     }
 
     @Test
