17b84e93488267dcd92807e25fdee53fd2b87663
Anton <voevodin_anton_2000@mail.ru>
2020-12-06 15:32:27 +0400

    АВТОМАТический апгрейд в6.2 Добавил абстрактную модель таблицы

--- src/main/java/ru/ssau/tk/vaa/LR_Voevodin_Kashapov/operations/IntegratingExecutor.java
+++ src/main/java/ru/ssau/tk/vaa/LR_Voevodin_Kashapov/operations/IntegratingExecutor.java
@@ -1,5 +1,6 @@
 package ru.ssau.tk.vaa.LR_Voevodin_Kashapov.operations;
 
+import ru.ssau.tk.vaa.LR_Voevodin_Kashapov.functions.ConstantFunction;
 import ru.ssau.tk.vaa.LR_Voevodin_Kashapov.functions.LinkedListTabulatedFunction;
 import ru.ssau.tk.vaa.LR_Voevodin_Kashapov.functions.SinFunction;
 import ru.ssau.tk.vaa.LR_Voevodin_Kashapov.functions.TabulatedFunction;
@@ -11,9 +12,9 @@ import java.util.concurrent.*;
 public class IntegratingExecutor {
     public static void main(String[] args) {
         ForkJoinPool pool = new ForkJoinPool();
-        TabulatedFunction function = new LinkedListTabulatedFunction(new ConstantFunction(2), 0, 3, 10000);
+        TabulatedFunction function = new LinkedListTabulatedFunction(new ConstantFunction(2), 0, 6, 10000);
 
-        ForkJoinTask<Double> task = new IntegratingTask(function, 0, 3);
+        ForkJoinTask<Double> task = new IntegratingTask(function, 0, 6);
         System.out.println(pool.invoke(task));
     }
 }
---
+++ src/main/java/ru/ssau/tk/vaa/LR_Voevodin_Kashapov/ui/MyTableModel.java
@@ -0,0 +1,65 @@
+package ru.ssau.tk.vaa.LR_Voevodin_Kashapov.ui;
+
+import javax.swing.table.AbstractTableModel;
+import java.util.List;
+
+public class MyTableModel extends AbstractTableModel {
+    private static final int X_COLUMN = 0;
+    private static final int Y_COLUMN = 1;
+    private static final long serialVersionUID = -443916866115057318L;
+    private final List<String> xValues;
+    private final List<String> yValues;
+
+
+    public MyTableModel(List<String> xValues, List<String> yValues) {
+        this.xValues = xValues;
+        this.yValues = yValues;
+    }
+
+
+    @Override
+    public int getRowCount() {
+        return xValues.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 2;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        switch (columnIndex) {
+            case X_COLUMN:
+                return xValues.get(rowIndex);
+
+            case Y_COLUMN:
+                return yValues.get(rowIndex);
+
+        }
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void setValueAt(Object value, int rowIndex, int columnIndex) {
+        switch (columnIndex) {
+            case X_COLUMN -> xValues.set(rowIndex, String.valueOf(value));
+            case Y_COLUMN -> yValues.set(rowIndex, String.valueOf(value));
+        }
+    }
+
+    @Override
+    public boolean isCellEditable(int rowIndex, int columnIndex) {
+        return true;
+    }
+
+    @Override
+    public String getColumnName(int column) {
+        return switch (column) {
+            case X_COLUMN -> "X";
+            case Y_COLUMN -> "Y";
+            default -> super.getColumnName(column);
+        };
+    }
+
+}
